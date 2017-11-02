239d8a984e928262b6dffcd6b5d94fe9e9c9669b
Mitsichury <mitsichury@gmail.com>
2017-11-02 10:52:12 +0100

    Test unitaire base

--- backend/app.js
+++ backend/app.js
@@ -3,12 +3,18 @@ var app = require('express')(),
     io = require('socket.io').listen(server)
 
 let clients = []
-
+console.log("Starting socket")
 io.sockets.on('connection', function (socket) {
 
     socket.on("pseudo", pseudo => clients.push({pseudo: pseudo, socket: socket}))
 
-    socket.on('disconect', () => {clients.splice(clients.findIndex(elt => elt.socket === socket), 1)})
+    socket.on('disconect', () => {
+        clients.splice(clients.findIndex(elt => elt.socket === socket), 1)
+    })
 });
 
-server.listen(8081);
+server.listen(8081, () => {
+    console.log("listen on 8081")
+})
+
+export {clients}
