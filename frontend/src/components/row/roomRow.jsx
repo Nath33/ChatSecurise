5c91a25e1009e1a1984ecd1eb348c169d4f892ab
Unknown <eckan@live.Fr>
2017-11-12 03:18:43 +0100

    colorOnClickWIP

--- frontend/src/components/row/roomRow.jsx
+++ frontend/src/components/row/roomRow.jsx
@@ -1,20 +1,42 @@
 import React from 'react'
+import {subscribe} from './../../connectToSocket';
 
 export default class RoomRow extends React.Component {
 
 	constructor(props) {
 		super(props)
 
+		this.state = {
+			myRoom: '',
+		}
+	}
+	componentDidMount(){
+		subscribe('yourRoom', (jsonRoom) => {
+			this.setState({
+				myRoom: JSON.parse(jsonRoom),
+				messages: [],
+			})
+		})
+	}
+
+	componentWillUpdate(){
+		if(this.state.myRoom === this.props.room){document.getElementById(this.props.room).style.color = 'orange'}
+		else{document.getElementById(this.props.room).style.color = 'pink'}
+		console.log(this.state.myRoom)
 	}
 
 	handle = () => {
 		this.props.click(this.props.room)
+		this.setState({
+			myRoom: this.props.room
+		})
+		console.log(this.state.myRoom)
 	}
 
 	render() {
 		return (
 			<div>
-				<li onClick={this.handle}>
+				<li id={this.props.room} onClick={this.handle}>
 					{this.props.room}
 				</li>
 			</div>
